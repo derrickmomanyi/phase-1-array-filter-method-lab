2421f2699c3b323db143162c3f6758b4c0ef53de
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-28 22:19:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, name){
+    return drivers.filter(drivers => drivers.toLowerCase().includes(name.toLowerCase()))
+
+};
+
+function fuzzyMatch(drivers, stringOfDrivers){
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, stringOfDrivers.length) === stringOfDrivers.toLowerCase()
+    })
+    };
+    
+    
+
+
+function matchName(drivers, namesOfDrivers){
+    return drivers.filter((data) => data.name === namesOfDrivers);
+};
